f0c537fcc1fe8cf67e6d62019bbde0c0e7293cfb
Gusujja <unisonujja@gmail.com>
2024-11-21 00:57:48 +0500

    updated

--- src/AdminPortal/Sidebar/Sidebar.jsx
+++ src/AdminPortal/Sidebar/Sidebar.jsx
@@ -1,8 +1,6 @@
 import React, { useState} from "react";
 import styles from "./Sidebar.module.css"; 
-import { Link } from "react-router-dom";
-
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate  } from "react-router-dom";
 
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
