f6a9ae7b48c6c2b3770663c4d4fcfb561939c5df
Gusujja <unisonujja@gmail.com>
2024-11-23 00:03:45 +0500

    updated

--- src/pages/Classes/VideoCard/VideoDescription.jsx
+++ src/pages/Classes/VideoCard/VideoDescription.jsx
@@ -29,8 +29,7 @@ const VideoDescription = ({ heading,description,views,time }) => {
     <VideoDescriptionStyled>
    <h4 className="heading">{heading}</h4>
 <div className="views-and-likes">
-<p className="description">{String(description || "").split(" ").slice(0, 150).join(" ") + (description && String(description).split(" ").length > 150 ? "..." : "")}</p>
-
+<p className="description">{description.slice(0, 150) + "..."}</p>
   
 
 </div>
