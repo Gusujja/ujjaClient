94ccbcab6a7ff91dfe84d5435cc81ac0b5374b93
Gusujja <unisonujja@gmail.com>
2025-09-16 16:49:28 +0500

    updated

--- src/pages/Home/Joining/styles.js
+++ src/pages/Home/Joining/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import bgImg from "../../../assets/images/joining_bg.png";
+import bgImg from "../../../assets/images/image2.jpg";
 import {
   fontFamilyRegular,
   mediaDeviceMax,
